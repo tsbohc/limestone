d9959366708e762c2a76685393f37d4d65fe5620
sean <justawinter@gmail.com>
2021-08-05 13:16:15 +0300

    lsp stuff

--- lua/lush_theme/limestone.lua
+++ lua/lush_theme/limestone.lua
@@ -40,14 +40,10 @@ local base0E = hsluv(266,8,50) -- keywords, folds
 local base0F = hsluv(259,9,30) -- parens
 
 --- extra
+local pppp00 = hsluv(45,73,73)
 local red000 = hsluv(13,65,55)
 
 local bbbb99 = hsluv(258,84,70)
-local pppp00 = hsluv(45,73,73)
-
-
---local pppp00 = hsluv(351,80,65)
-
 
 
 --local bbbb99 = hsluv(258,100,67)
@@ -70,28 +66,39 @@ local test = hsluv(100, 100, 70)
 --local base0F = hsl() -- Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
 -- }}}
 
---local X = {
---  background = base00,
---  foreground = base08,
---  color0 = base00,
---  color1 = hsl(red000.hsl),
---  color2 = hsl(140, 25,  73),
---  color3 = hsl(40,  49,  73),
---  color4 = hsl(209, 40,  72),
---  color5 = hsl(285, 30,  76),
---  color6 = hsl(175, 36,  72),
---  color7 = base08,
---  color8 = base08,
---  color9 = hsl(345, 53,  77),
---  color10 = hsl(140, 25,  73),
---  color11 = hsl(40,  49,  73),
---  color12 = hsl(209, 40,  72),
---  color13 = hsl(285, 30,  76),
---  color14 = hsl(175, 36,  72),
---  color15 = base08,
---}
---
+local darken = 10
 
+local colors = {
+  background = hsluv(base00.hex),
+  foreground = hsluv(base08.hex),
+  -- black
+  color0 = hsluv(base00.hex),
+  color8 = hsluv(base03.hex),
+  -- red
+  color1 = hsluv(red000.de(10).hex),
+  color9 = hsluv(red000.de(30).hex),
+  -- green
+  color2 = hsluv(base0D.da(darken).de(10*darken).hex),
+  color10 = hsluv(base0D.da(darken).de(10*darken).hex),
+  ---- yellow
+  color3 = hsluv(base09.da(darken).hex),
+  color11 = hsluv(base09.hex),
+  ---- blue
+  color4 = hsluv(base0E.da(darken).hex),
+  color12 = hsluv(base0E.hex),
+  ---- magenta
+  color5 = hsluv(base0F.da(darken).hex),
+  color13 = hsluv(base0F.hex),
+  ---- cyan
+  color6 = hsluv(base0B.da(darken).hex),
+  color14 = hsluv(base0B.hex),
+  -- white
+  color7 = hsluv(base08.hex),
+  color15 = hsluv(base0D.hex),
+}
+
+
+-- {{{
 --local X = {
 --  background = base00,
 --  foreground = base08,
@@ -112,9 +119,11 @@ local test = hsluv(100, 100, 70)
 --  white = ,
 --  b_white = ,
 --}
+--}}}
 
 local theme = lush(function()
   return {
+    X { lush = colors },
     --X {
     --  background = base00,
     --  foreground = base08,
@@ -154,7 +163,6 @@ local theme = lush(function()
     LightspeedLabelDistant { fg = test },
     LightspeedOneCharMatch { fg = test },
     LightspeedUniqueChar { fg = test },
-
 
     Comment      { fg = base03, gui = italic }, -- any comment
     ColorColumn  { bg = base01 }, -- used for the columns set with 'colorcolumn'
@@ -281,30 +289,30 @@ local theme = lush(function()
     -- LspReferenceRead                     { }, -- used for highlighting "read" references
     -- LspReferenceWrite                    { }, -- used for highlighting "write" references
 
-    -- LspDiagnosticsDefaultError           { }, -- Used as the base highlight group. Other LspDiagnostic highlights link to this by default (except Underline)
-    -- LspDiagnosticsDefaultWarning         { }, -- Used as the base highlight group. Other LspDiagnostic highlights link to this by default (except Underline)
-    -- LspDiagnosticsDefaultInformation     { }, -- Used as the base highlight group. Other LspDiagnostic highlights link to this by default (except Underline)
-    -- LspDiagnosticsDefaultHint            { }, -- Used as the base highlight group. Other LspDiagnostic highlights link to this by default (except Underline)
+    LspDiagnosticsDefaultError           { fg = base03.mix(red000, 30), gui = "italic" }, -- Used as the base highlight group. Other LspDiagnostic highlights link to this by default (except Underline)
+    LspDiagnosticsDefaultWarning         { fg = base03.mix(pppp00, 30), gui = "italic" }, -- Used as the base highlight group. Other LspDiagnostic highlights link to this by default (except Underline)
+    LspDiagnosticsDefaultInformation     { fg = base03, gui = "italic" }, -- Used as the base highlight group. Other LspDiagnostic highlights link to this by default (except Underline)
+    LspDiagnosticsDefaultHint            { fg = base03, gui = "italic" }, -- Used as the base highlight group. Other LspDiagnostic highlights link to this by default (except Underline)
 
     -- LspDiagnosticsVirtualTextError       { }, -- Used for "Error" diagnostic virtual text
     -- LspDiagnosticsVirtualTextWarning     { }, -- Used for "Warning" diagnostic virtual text
     -- LspDiagnosticsVirtualTextInformation { }, -- Used for "Information" diagnostic virtual text
     -- LspDiagnosticsVirtualTextHint        { }, -- Used for "Hint" diagnostic virtual text
 
-    -- LspDiagnosticsUnderlineError         { }, -- Used to underline "Error" diagnostics
-    -- LspDiagnosticsUnderlineWarning       { }, -- Used to underline "Warning" diagnostics
-    -- LspDiagnosticsUnderlineInformation   { }, -- Used to underline "Information" diagnostics
-    -- LspDiagnosticsUnderlineHint          { }, -- Used to underline "Hint" diagnostics
+    LspDiagnosticsUnderlineError         { bg = base01 }, -- Used to underline "Error" diagnostics
+    LspDiagnosticsUnderlineWarning       { bg = base01 }, -- Used to underline "Warning" diagnostics
+    LspDiagnosticsUnderlineInformation   { bg = base01 }, -- Used to underline "Information" diagnostics
+    LspDiagnosticsUnderlineHint          { bg = base01 }, -- Used to underline "Hint" diagnostics
 
     -- LspDiagnosticsFloatingError          { }, -- Used to color "Error" diagnostic messages in diagnostics float
     -- LspDiagnosticsFloatingWarning        { }, -- Used to color "Warning" diagnostic messages in diagnostics float
     -- LspDiagnosticsFloatingInformation    { }, -- Used to color "Information" diagnostic messages in diagnostics float
     -- LspDiagnosticsFloatingHint           { }, -- Used to color "Hint" diagnostic messages in diagnostics float
 
-    -- LspDiagnosticsSignError              { }, -- Used for "Error" signs in sign column
-    -- LspDiagnosticsSignWarning            { }, -- Used for "Warning" signs in sign column
-    -- LspDiagnosticsSignInformation        { }, -- Used for "Information" signs in sign column
-    -- LspDiagnosticsSignHint               { }, -- Used for "Hint" signs in sign column
+    LspDiagnosticsSignError              { fg = red000 }, -- Used for "Error" signs in sign column
+    LspDiagnosticsSignWarning            { fg = pppp00 }, -- Used for "Warning" signs in sign column
+    LspDiagnosticsSignInformation        { fg = base0E }, -- Used for "Information" signs in sign column
+    LspDiagnosticsSignHint               { fg = base0E }, -- Used for "Hint" signs in sign column
 
     -- These groups are for the neovim tree-sitter highlights.
     -- As of writing, tree-sitter support is a WIP, group names may change.
